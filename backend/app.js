4115c8ea1f0204cf6a9e4a44423d3a30d5c3e7c4
mambitoano2000 <joaocostagaspar@gmail.com>
2021-07-09 16:07:49 +0200

    normalizePort + errorHanlder + eventlistener

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
+   res.json({ message: 'Votre requête a bien été reçue !!!' }); 
 });
 
 module.exports = app;
