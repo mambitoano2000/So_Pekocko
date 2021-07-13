43c60b9cef9b6a79426e7bccb12118d9090af08c
mambitoano2000 <joaocostagaspar@gmail.com>
2021-07-13 16:47:45 +0200

    added put / modification sauce

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -42,3 +42,14 @@ exports.createSauce = (req, res, next) => {
       }
     );
   };
+
+  exports.modificationSauce = (req, res, next) => {
+    const sauceObject = req.file ?
+    {
+      ...JSON.parse(req.body.sauce),
+      imageUrl : `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body};
+    Sauce.updateOne({_id : req.params.id}, {...sauceObject, _id : req.params.id})
+      .then(sauces => res.status(200).json({message : "modification sauce"}))
+      .catch(error => res.status(400).json({error}))
+};
--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -9,7 +9,7 @@ const multer = require('../middleware/multer-config');
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.get('/', auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
-//router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.put('/:id', auth, multer, sauceCtrl.modificationSauce);
 //router.delete('/:id', auth, sauceCtrl.deleteSauce);
 //router.post('/:id/like', auth, sauceCtrl.likeOrDislikeSauce);
 
